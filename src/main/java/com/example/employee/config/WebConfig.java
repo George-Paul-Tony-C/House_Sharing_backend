1dbdd4e5716109a8ba76a2a0e36b0cc48629a60d
George-Paul-Tony-C <171569996+George-Paul-Tony-C@users.noreply.github.com>
2025-05-14 10:32:23 +0530

    Ready to Host

--- src/main/java/com/example/employee/config/WebConfig.java
+++ src/main/java/com/example/employee/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") 
                 .allowedOrigins("http://localhost:5173/") 
+                // .allowedOrigins("https://house-sharing.vercel.app/") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE") 
                 .allowedHeaders("*") 
                 .allowCredentials(true); 
